36a3a238a81b8c379c869f6be63539d48c8737e3
Md. Mahbubur Rahman <semahbub@gmail.com>
2020-04-18 18:21:02 +0600

    Initial project files added on April 18, 2020

---
+++ App07_ModelValidation/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using App07_ModelValidation.Models;
+
+namespace App07_ModelValidation.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext (DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<App07_ModelValidation.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ App07_ModelValidation/Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App07_ModelValidation.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name="Employee Name")]
+        public string Name { get; set; }
+
+        [Required, MinLength(5)]
+        public string Designation { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Date), Display(Name="Date of Joining")]
+        public DateTime JoiningDate { get; set; }
+
+        [Required, DataType(DataType.Date), Display(Name = "Date of Birth")]
+        public DateTime BirthDate { get; set; }   
+
+        [Range(0, 300000)]
+        public double BasicSalary { get; set; }
+
+        [Required]
+        public bool IsEligibleforBonus { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Grade { get; set; }
+
+        public bool IsPartTime { get; set; }
+
+        [Url]
+        public string Website { get; set; }
+    }
+}
